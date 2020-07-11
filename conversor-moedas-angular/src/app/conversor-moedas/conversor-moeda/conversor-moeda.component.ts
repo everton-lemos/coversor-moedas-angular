fb5e2ae118d9982975bedb23fba884f8ea637718
condor <e.hhdelemos@gmail.com>
2020-07-11 18:17:30 -0300

    Cria o component

---
+++ conversor-moedas-angular/src/app/conversor-moedas/conversor-moeda/conversor-moeda.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-conversor-moeda',
+  templateUrl: './conversor-moeda.component.html',
+  styleUrls: ['./conversor-moeda.component.css']
+})
+export class ConversorMoedaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
