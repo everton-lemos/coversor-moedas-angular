e029bfea9ca0a3a824f426c4d67d6fdabacdac0d
condor <e.hhdelemos@gmail.com>
2020-07-11 18:16:21 -0300

    cria o modulo

--- conversor-moedas-angular/src/app/conversor-moedas/conversor-moedas.module.ts
+++ conversor-moedas-angular/src/app/conversor-moedas/conversor-moedas.module.ts
@@ -1,12 +1,13 @@
 import { ConversorMoedasRoutingModule } from './conversor-moedas.routing';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ConversorMoedaComponent } from './conversor-moeda/conversor-moeda.component';
 
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ConversorMoedaComponent],
   imports: [
     CommonModule,
     ConversorMoedasRoutingModule
