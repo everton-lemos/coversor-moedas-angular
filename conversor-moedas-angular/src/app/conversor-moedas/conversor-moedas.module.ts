b7070a7dbe66b960799fc80615a75d213de7dbe1
condor <e.hhdelemos@gmail.com>
2020-07-11 15:48:59 -0300

    Cria o projeto

---
+++ conversor-moedas-angular/src/app/conversor-moedas/conversor-moedas.module.ts
@@ -0,0 +1,15 @@
+import { ConversorMoedasRoutingModule } from './conversor-moedas.routing';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ConversorMoedasRoutingModule
+  ]
+})
+export class ConversorMoedasModule { }
