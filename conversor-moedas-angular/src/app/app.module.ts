776da547a268714d03b4e81ef237c01dc84e27d8
condor <e.hhdelemos@gmail.com>
2020-07-11 20:07:20 -0300

    Cria servico moeda

--- conversor-moedas-angular/src/app/app.module.ts
+++ conversor-moedas-angular/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ConversorMoedasModule } from './conversor-moedas/conversor-moedas.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ConversorMoedasModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- conversor-moedas-angular/src/app/conversor-moedas/conversor-moedas.module.ts
+++ conversor-moedas-angular/src/app/conversor-moedas/conversor-moedas.module.ts
@@ -1,7 +1,10 @@
+import { HttpClientModule } from '@angular/common/http';
+import { ConversorMoedasService } from './conversor-moedas.service';
 import { ConversorMoedasRoutingModule } from './conversor-moedas.routing';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConversorMoedaComponent } from './conversor-moeda/conversor-moeda.component';
+import { FormsModule } from '@angular/forms';
 
 
 
@@ -10,7 +13,11 @@ import { ConversorMoedaComponent } from './conversor-moeda/conversor-moeda.compo
   declarations: [ConversorMoedaComponent],
   imports: [
     CommonModule,
-    ConversorMoedasRoutingModule
-  ]
+    ConversorMoedasRoutingModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  exports: [ConversorMoedaComponent],
+  providers: [ConversorMoedasService]
 })
 export class ConversorMoedasModule { }
